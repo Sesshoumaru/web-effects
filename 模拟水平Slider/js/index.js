f5e00e558579f8c36cb68cda82e69779731db1ff
Sesshoumaru <laobi.net@live.com>
2017-10-18 13:53:10 +0800

    add horizontal slider effect

---
+++ 模拟水平Slider/js/index.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+    var scroll = document.getElementById("scroll");
+    var bar = scroll.children[0];
+    var mask = scroll.children[1];
+    var message = scroll.nextElementSibling || scroll.nextSibling;
+
+    bar.onmousedown = function (event) {
+        var event = event || window.event;
+        var leftVal = event.clientX - this.offsetLeft;
+
+        document.onmousemove = function () {
+            var event = event || window.event;
+            var x = event.clientX - leftVal;
+            if(x<0)
+                x=0;
+            else if(x> scroll.offsetWidth)
+                x= scroll.offsetWidth;
+
+
+            bar.style.left =  x+ "px";
+            mask.style.width =  bar.style.left;
+            message.innerHTML = "百分比："+  parseInt(x* 100 /scroll.offsetWidth)+ "%";
+
+            window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
+        }
+    };
+    bar.onmouseup = function () {
+        document.onmousemove = null;
+    }
+};
